f467bb460137d822f91f65240ef77babbd4b1401
NYHolzer <nissan.holzer@gmail.com>
2019-09-05 22:18:16 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
 
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.create(product_params)
+        @product.save
+        redirect_to products_path
+    end
+
+    def description
+        find_product
+        render plain: @product.description
+    end
+
+    def inventory
+        find_product 
+        if @product.inventory > 0
+            render plain: "true"
+        else 
+            render plain: "false"
+        end
+    end
+    private 
+    def product_params
+        params.require(:product).permit(:name, :description)
+    end
+
+    def find_product
+        @product = Product.find(params[:id])
+    end
 end
